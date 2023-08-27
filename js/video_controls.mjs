372a98185608e51c281bd8fcaf8c02ca8ef513e3
Martin Tuskevicius <martin.tuskevicius@gmail.com>
2023-08-27 01:29:07 -0700

    Slightly improved the style of the video controls.

--- js/video_controls.mjs
+++ js/video_controls.mjs
@@ -6,26 +6,14 @@ const muteButton = document.getElementById("mute-button");
 videoPlayer.ontimeupdate = () => progressBar.value = videoPlayer.currentTime / videoPlayer.duration;
 
 playButton.onmouseup = () => {
-  toggleState(playButton, "play", "pause", () => {
-    videoPlayer.play();
-    playButton.textContent = "Pause";
-  }, () => {
-    videoPlayer.pause();
-    playButton.textContent = "Play";
-  });
+  toggleState(playButton, "play", "pause", () => videoPlayer.play(), () => videoPlayer.pause());
 };
 
 muteButton.onmouseup = () => {
-  toggleState(muteButton, "mute", "unmute", () => {
-    videoPlayer.muted = true;
-    muteButton.textContent = "Unmute";
-  }, () => {
-    videoPlayer.muted = false;
-    muteButton.textContent = "Mute";
-  });
+  toggleState(muteButton, "mute", "unmute", () => videoPlayer.muted = true, () => videoPlayer.muted = false);
 };
 
-function toggleState(element, stateOne, stateTwo, stateOneCallback = {}, stateTwoCallback = {}) {
+function toggleState(element, stateOne, stateTwo, stateOneCallback = () => {}, stateTwoCallback = () => {}) {
   if (element.getAttribute("data-state") === stateOne) {
     stateOneCallback();
     element.setAttribute("data-state", stateTwo);
