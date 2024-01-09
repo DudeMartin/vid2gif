3c3f9f0a7fc18cf71669e17b111cc7b280eed70a
Martin Tuskevicius <martin.tuskevicius@gmail.com>
2024-01-08 23:07:20 -0800

    Fixed an issue that caused recordings to have the wrong resolution.
    When recording uncropped videos, the resolution of the resulting GIF would be the resolution of the video player in the browser rather than the resolution of the video that was playing.

--- js/gif_recorder.mjs
+++ js/gif_recorder.mjs
@@ -2,11 +2,34 @@ export function startRecording(videoPlayer, options = {}) {
   if (!options.frameRate) {
     options.frameRate = 5;
   }
-  if (options.crop) {
-    return startCroppedRecording(videoPlayer, options);
-  } else {
-    return startFullRecording(videoPlayer, options);
+  if (!options.crop) {
+    options.crop = [0, 0, 1, 1];
   }
+  const recordX = Math.floor(options.crop[0] * videoPlayer.videoWidth);
+  const recordY = Math.floor(options.crop[1] * videoPlayer.videoHeight);
+  const recordWidth = Math.floor(options.crop[2] * videoPlayer.videoWidth);
+  const recordHeight = Math.floor(options.crop[3] * videoPlayer.videoHeight);
+  const offscreenContext = new OffscreenCanvas(videoPlayer.videoWidth, videoPlayer.videoHeight).getContext("2d");
+  const frameDelayMillis = 1000 / options.frameRate;
+  const animatedGif = new Animated_GIF({
+    width: recordWidth,
+    height: recordHeight,
+    delay: frameDelayMillis
+  });
+  initializeDetails(recordWidth, recordHeight, options.frameRate);
+  const intervalId = setInterval(() => {
+    if (!isVideoPlaying(videoPlayer)) {
+      return;
+    }
+    offscreenContext.drawImage(videoPlayer, 0, 0);
+    animatedGif.addFrameImageData(offscreenContext.getImageData(recordX, recordY, recordWidth, recordHeight));
+    countFrame();
+  }, frameDelayMillis);
+  return {
+    options: options,
+    recorder: animatedGif,
+    intervalId: intervalId
+  };
 }
 
 export function stopRecording(context) {
@@ -18,75 +41,6 @@ export function stopRecording(context) {
       blob: blob
     });
   }));
-}
-
-function startCroppedRecording(videoPlayer, options) {
-  const cropX = Math.floor(options.crop[0] * videoPlayer.videoWidth);
-  const cropY = Math.floor(options.crop[1] * videoPlayer.videoHeight);
-  const width = Math.floor(options.crop[2] * videoPlayer.videoWidth);
-  const height = Math.floor(options.crop[3] * videoPlayer.videoHeight);
-  const offscreenContext = new OffscreenCanvas(videoPlayer.videoWidth, videoPlayer.videoHeight).getContext("2d");
-  const frameDelay = 1000 / options.frameRate;
-  const animatedGif = new Animated_GIF({
-    width: width,
-    height: height,
-    delay: frameDelay
-  });
-  initializeDetails(width, height, options.frameRate);
-  const intervalId = setInterval(() => {
-    if (!isVideoPlaying(videoPlayer)) {
-      return;
-    }
-    offscreenContext.drawImage(videoPlayer, 0, 0);
-    animatedGif.addFrameImageData(offscreenContext.getImageData(cropX, cropY, width, height));
-    countFrame();
-  }, frameDelay);
-  return {
-    options: options,
-    recorder: animatedGif,
-    intervalId: intervalId
-  };
-}
-
-function startFullRecording(videoPlayer, options) {
-  const [width, height] = calculateSize(videoPlayer, options.width, options.height);
-  const frameDelay = 1000 / options.frameRate;
-  const animatedGif = new Animated_GIF({
-    width: width,
-    height: height,
-    delay: frameDelay
-  });
-  initializeDetails(width, height, options.frameRate);
-  const intervalId = setInterval(() => {
-    if (!isVideoPlaying(videoPlayer)) {
-      return;
-    }
-    animatedGif.addFrame(videoPlayer);
-    countFrame();
-  }, frameDelay);
-  return {
-    options: options,
-    recorder: animatedGif,
-    intervalId: intervalId
-  };
-}
-
-function calculateSize(videoPlayer, width, height) {
-  if (width && height) {
-    return [width, height];
-  } else if (width) {
-    return [width, videoPlayer.videoHeight * width / videoPlayer.videoWidth]
-  } else if (height) {
-    return [videoPlayer.videoWidth * height / videoPlayer.videoHeight, height];
-  } else {
-    let scaledWidth = videoPlayer.videoWidth;
-    let scaledHeight = videoPlayer.videoHeight;
-    for (let factor = 1; Math.max(scaledWidth, scaledHeight) > 1000; factor += 0.5) {
-      scaledWidth = Math.round(videoPlayer.videoWidth / factor);
-      scaledHeight = Math.round(videoPlayer.videoHeight / factor);
-    }
-    return [scaledWidth, scaledHeight];
-  }
 }
 
 function initializeDetails(width, height, frameRate) {
